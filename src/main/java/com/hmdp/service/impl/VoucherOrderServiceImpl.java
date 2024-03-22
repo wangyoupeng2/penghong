1cfca8d8d51230779329611786eedce4f08fbba5
31694 <3169468598@qq.com>
2024-03-22 20:40:59 +0800

    单机条件下实现秒杀卷一人一卷的悲观锁实现，使用AopContext获取代理对象够调用锁函数

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -11,6 +11,7 @@ import com.hmdp.service.IVoucherService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisInWork;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,7 +36,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private ISeckillVoucherService seckillVoucherService;
     @Override
-    @Transactional
+
     public Result seckillVoucher(Long voucherId) {
         //查询ID
         Voucher voucher = iVoucherService.getById(voucherId);
@@ -49,31 +50,50 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (voucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
-        //实现一人一单
-        Integer count = this.query().eq("voucher_id", voucherId).eq("user_id", userID).count();
-        if(count > 0){
-            return Result.fail("单一用户只允许一张");
+        //原本的锁使用方法，有可能在没有提交的时候就又被获取到锁,因此将锁加在这里
+        synchronized (userID.toString().intern()) {
+            //实现一人一单,因为同时存在并发问题，但是有无法进行条件判断更新，所以只能使用悲观锁
+
+            /*return createVoucherOrder(voucherId);*/
+            //出现一个问题，因为事务管理加在这个函数上
+            //但是因为该函数调用是使用this指向，没有事物能力，所以需要寻找代理对象调用函数
+
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId ,userID);
+            //获取事务代理对象，使用代理对象调用，函数要在Serive里面声明
         }
-        //如果是，判断库存，库存-1，返回订单账号，创建订单
-        boolean success = seckillVoucherService.update().setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
-                //CAS乐观锁，对stock进行version判断，如果和最开始数据不同就不更新
-                /*.eq("stock",voucher.getStock()).update();*/
-
-                //但是使用乐观锁时，条件太谨慎，会导致大多数人同时进入时失败，
-                //因此只需要条件库存大于0就可以了
-                .gt("stock",0).update();
-
-        if (!success) {
-            return Result.fail("库存不足");
-        }
-        VoucherOrder voucherOrder = new VoucherOrder();
-        voucherOrder.setVoucherId(voucherId);
-        voucherOrder.setId(redisInWork.nextID("order"));
-        voucherOrder.setUserId(UserHolder.getUser().getId());
-        save(voucherOrder);
-
-        //返回订单id
-        return Result.ok(voucherOrder.getId());
     }
+
+    @Transactional
+    public  Result createVoucherOrder(Long voucherId,Long userID) {
+        //使用悲观锁，将每一个userid作为锁，只有同一个用户来才加锁，所以锁加在了userID，不在函数上
+/*      1  Long userID = UserHolder.getUser().getId();*/
+/*      2  synchronized (userID.toString().intern())*/
+        //userId,tostring.intern在线程池中寻找字符串对应的对象
+            Integer count = this.query().eq("voucher_id", voucherId).eq("user_id", userID).count();
+            if (count > 0) {
+                return Result.fail("单一用户只允许一张");
+            }
+            //如果是，判断库存，库存-1，返回订单账号，创建订单
+            boolean success = seckillVoucherService.update().setSql("stock = stock - 1")
+                    .eq("voucher_id", voucherId)
+                    //CAS乐观锁，对stock进行version判断，如果和最开始数据不同就不更新
+                    /*.eq("stock",voucher.getStock()).update();*/
+
+                    //但是使用乐观锁时，条件太谨慎，会导致大多数人同时进入时失败，
+                    //因此只需要条件库存大于0就可以了
+                    .gt("stock", 0).update();
+
+            if (!success) {
+                return Result.fail("库存不足");
+            }
+            VoucherOrder voucherOrder = new VoucherOrder();
+            voucherOrder.setVoucherId(voucherId);
+            voucherOrder.setId(redisInWork.nextID("order"));
+            voucherOrder.setUserId(UserHolder.getUser().getId());
+            save(voucherOrder);
+
+            //返回订单id
+            return Result.ok(voucherOrder.getId());
+        }
 }
